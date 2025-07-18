abd4d7d6dd3943c77c5a4795d896d582bad6fe92
Kars <k@kars.bio>
2025-07-18 07:18:49 +0100

    initial variable practice

--- hello-world.cpp
+++ hello-world.cpp
@@ -4,4 +4,4 @@ int main() {
   std::cout << "Hello World!";
   return 0;
 }
-
+:!g++ hello-world.cpp -o main > compile.log:!type compile.log 2>&1
---
+++ variable-init.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  int a, b;             // Init  2 variablex, a, b of int
+
+  a = 5;
+  std::cout << a;
+
+  a = 7;                // Variables are immutabel :3
+  std::cout << a;
+  return 0;
+}
