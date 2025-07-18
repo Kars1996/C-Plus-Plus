b76a1ce2a972516ce628adecf26145ccb2af5ffb
Kars <k@kars.bio>
2025-07-18 07:22:48 +0100

    new func

--- variable-init.cpp
+++ variable-init.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+
+int initializatoin_tests() 
+{
+  // btw i dont know if this causes a compile error
+  // haven't got to the functions bit of the tutorial
+
+  int a; // no default value
+
+  // Old initialization stuff 
+  int b = 5;
+  int c ( 6 );
+
+  // new initialization stuff
+  int d { 7 };
+  int e {};
+}
+
 int main()
 {
   int a, b;             // Init  2 variablex, a, b of int
-
   int width { 10 };     // Width has default var of 10
   a = 5;
   std::cout << a;
